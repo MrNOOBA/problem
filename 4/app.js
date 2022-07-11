1c48ce113fba10f3767038ee72d1375887cc2a94
Noah Wahlström <noah.wahlstrom@icloud.com>
2022-07-11 15:27:48 +0200

    5

--- 4/app.js
+++ 4/app.js
@@ -1,10 +1,11 @@
 document.getElementById("submitButton").onclick = function () {
     var n = window.document.getElementById("numberInput").value;
     let sum = 0;
-    for (i=0;i<=n;i++) {
-        sum += i
+    for (i = 0; i <= n; i++) {
+        if (i % 5 === 0 || i % 3 === 0) {
+            sum += i
+        }
     }
-
     parseInt(sum)
     alert(sum)
 }
