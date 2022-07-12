1146adfa2eb3df5d0fbd1eb19a22bb0093ca92f4
Noah Wahlström <noah.wahlstrom@icloud.com>
2022-07-12 13:30:59 +0200

    8

--- 8/app.js
+++ 8/app.js
@@ -1,5 +1,5 @@
-for (i = 1; i < 0; i++) {
+for (i = 2; i > 0 ; i++) {
     if (i % 2 !== 0 && i % 3 !== 0 && i % 5 !== 0 || i === 2 || i === 3 || i === 7) {
-        document.write(i)
+        document.write(i + " ")
     }
 }
